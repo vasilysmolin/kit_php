81c76ee7e09d2c8ced22ccda75520858d0424f6c
Eugene Kremlev <kremlev@hey.com>
2021-08-23 16:15:32 +0500

    add csrf-param token -> head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="description" content="@yield('description', 'Тапиго — это платформа, которая позволяет вам из одного места решать разные задачи, используя все преимущества единого аккаунта. Для людей и для компаний.')">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,400;1,700;1,800&family=Roboto:wght@400;700;900&family=Montserrat:wght@900&display=swap" rel="stylesheet">
