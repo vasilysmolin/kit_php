ac9b7d5f46596763a9311c296ae659a4d5bd0bb4
Eugene Kremlev <kremlev@hey.com>
2021-08-27 17:40:09 +0500

    +1 fix path register

--- resources/views/pages/promo.blade.php
+++ resources/views/pages/promo.blade.php
@@ -7,7 +7,7 @@
     <main class="container mt-[15px] lg:mt-[30px] flex flex-col justify-start items-center 2xl">
         @include('layouts.modules.messages')
         <h1 class="relative w-full px-5 xl:w-10/12 xl:p-0 font-mr font-black text-6 text-center leading-[1.15]">Доставка еды — как раз, два, три.</h1>
-        <a href="https://user.tapigo.ru/user/register" class="btn-create-account">Создать аккаунт</a>
+        <a href="https://user.tapigo.ru/register" class="btn-create-account">Создать аккаунт</a>
         <section class="relative flex flex-col justify-center items-center w-full box-border">
             <img src="/img/promo-1.webp" alt="Доставка еды" class="relative z-10 rellax" data-rellax-speed="5" data-rellax-mobile-speed="2">
             <p class="relative -mt-20 xs:-mt-24 sm:-mt-32 md:-mt-48 xl:-mt-72 font-mr font-black text-hybrid1 xs:text-[7.15rem] sm:text-[10.15rem] md:text-[12.2rem] lg:text-[16.3rem] xl:text-[20.5rem] 2xl:text-[24.65rem] text-gray-300 uppercase leading-none z-0">Скоро</p>
@@ -18,6 +18,6 @@
             <button type="submit" class="btn-submit">Сообщите мне сразу</button>
         </form>
         <p class="relative px-4 lg:max-w-screen-lg xs:text-[1.2rem] lg:text-[1.875rem] xs:text-center leading-normal">Тапиго — это платформа, которая позволяет вам из одного места решать разные задачи, используя все преимущества <a href="https://tapigo.ru/about" class="links">единого аккаунта</a>. Для людей и для компаний.</p>
-        <a href="https://user.tapigo.ru/user/register" class="btn-create-account">Создать аккаунт</a>
+        <a href="https://user.tapigo.ru/register" class="btn-create-account">Создать аккаунт</a>
     </main>
 @endsection
