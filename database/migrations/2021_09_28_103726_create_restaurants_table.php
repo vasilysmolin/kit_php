b679adb09b9754a087848d829e34f6a9c835b0d4
vasily.smolin <vasilysmoolin@gmail.com>
2021-10-01 10:19:19 +0400

    fix migrate

--- database/migrations/2021_09_28_103726_create_restaurants_table.php
+++ database/migrations/2021_09_28_103726_create_restaurants_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateRestaurantsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations..
      *
      * @return void
      */
