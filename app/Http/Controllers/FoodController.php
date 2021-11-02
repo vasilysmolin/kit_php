20094b58ac90e8f747c10e4119b985c0b2786c22
vasily.smolin <vasilysmoolin@gmail.com>
2021-11-02 19:03:42 +0400

    fix

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -185,18 +185,18 @@ class FoodController extends Controller
 
         $files = resolve(Files::class);
 
-        if (isset($request['files']) && count($request['files']) > 0) {
-            foreach ($request['files'] as $file) {
-                $dataFile = $files->preparationFileS3($file);
-                $restaurantFood->image()->create([
-                    'mimeType' => $dataFile['mineType'],
-                    'extension' => $dataFile['extension'],
-                    'name' => $dataFile['name'],
-                    'uniqueValue' => $dataFile['name'],
-                    'size' => $dataFile['size'],
-                ]);
-            }
-        }
+//        if (isset($request['files']) && count($request['files']) > 0) {
+//            foreach ($request['files'] as $file) {
+//                $dataFile = $files->preparationFileS3($file);
+//                $restaurantFood->image()->create([
+//                    'mimeType' => $dataFile['mineType'],
+//                    'extension' => $dataFile['extension'],
+//                    'name' => $dataFile['name'],
+//                    'uniqueValue' => $dataFile['name'],
+//                    'size' => $dataFile['size'],
+//                ]);
+//            }
+//        }
 
         return response()->json([], 201);
     }
