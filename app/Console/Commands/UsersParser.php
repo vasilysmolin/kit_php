10e84e0adccda2c93f1afb7dc100f4a94dbe1e4e
vasily.smolin <vasilysmoolin@gmail.com>
2022-01-03 14:31:50 +0400

    парсинг категорий у услуг

--- app/Console/Commands/UsersParser.php
+++ app/Console/Commands/UsersParser.php
@@ -47,116 +47,116 @@ class UsersParser extends Command
      */
     public function handle()
     {
-//        $client = new Client();
-//        $response = $client->get('https://user.tapigo.ru/all-users-json', ['verify' => false]);
-//        $contents = $response->getBody()->getContents();
-//        $contents = json_decode($contents, true);
-//        foreach ($contents as $item) {
-//            $userDB = User::find($item['id']);
-//            if (isset($userDB)) {
-//                $user = $userDB;
-//                $user->id = $item['id'];
-//                $user->email = $item['email'];
-//                $user->password = $item['password'];
-//                $user->phone = $item['phone'];
-//                $user->email_verified_at = $item['email_verified_at'];
-//                $user->name = $item['profile'] ? $item['profile']['name'] : null;
-//                $user->update();
-//            } else {
-//                $count = User::where('email', $item['email'])
-//                    ->orWhere('phone', $item['phone'])
-//                    ->get();
-//
-//                if ($count->count() === 0) {
-//                    $user = new User();
-//                    $user->id = $item['id'];
-//                    $user->email = $item['email'];
-//                    $user->password = $item['password'];
-//                    $user->phone = $item['phone'];
-//                    $user->email_verified_at = $item['email_verified_at'];
-//                    $user->name = $item['profile'] ? $item['profile']['name'] : null;
-//                    $user->save();
-//                    $user->profile()->create(['id' => $item['profile']['id']]);
-//                }
-//            }
-//        }
-//
-//        $client = new Client();
-//        $response = $client->get('https://catalog.tapigo.ru/all-resume-json', ['verify' => false]);
-//        $contents = $response->getBody()->getContents();
-//        $contents = json_decode($contents, true);
-//
-//        foreach ($contents as $item) {
-//            $userDB = User::find($item['id']);
-//            if(!isset($userDB)) {
-//                continue;
-//            }
-//            $profileID = $userDB->profile->getKey();
-//            if (isset($item['posts']) && !empty($item['posts'])) {
-//                foreach ($item['posts'] as $post) {
-//                    if (isset($post['property']) && !empty($post['property'])) {
-//                        if ($post['property_type'] === "App\\Models\\Jobs\\Resume\\JobsPostResumeProperty") {
-//                            $property = $post['property'];
-//                            $resume = JobsResume::find($property['id']);
-//                            $slug = Str::slug($property['title'] .  ' ' . Str::random(5), '-');
-//                            if ($resume) {
-//                                $model = $resume;
-//                            } else {
-//                                $model = new JobsResume();
-//                            }
-//                            $model->id = $property['id'];
-//                            $model->price = $property['price'];
-//                            $model->description = $property['desc'];
-//                            $model->title = $property['title'];
-//                            $model->experience = (new TimeArray($property['experience_years'], null))->parce();
-//                            $model->education = (new Education($property['education'], null))->parce();
-//                            $model->schedule = (new Education($property['schedule'], null))->parce();
-//                            $model->salary_type = (new SalaryType($property['salary_type'] ?? null, null))->parce();
-//                            $model->alias = $slug;
-//                            $model->active = true;
-//                            $model->profile_id = $profileID;
-//
-//                            if ($resume) {
-//                                $model->update();
-//                            } else {
-//                                $model->save();
-//                            }
-//                        }
-//                    }
-//                    if ($post['property_type'] === "App\\Models\\Jobs\\Vacancy\\JobsPostVacancyProperty") {
-//                        $property = $post['property'];
-//                        $resume = JobsVacancy::find($property['id']);
-//                        $slug = Str::slug($property['title'] .  ' ' . Str::random(5), '-');
-//                        if ($resume) {
-//                            $model = $resume;
-//                        } else {
-//                            $model = new JobsVacancy();
-//                        }
-//                        $model->id = $property['id'];
-//                        $model->min_price = $property['price'];
-//                        $model->max_price = $property['price_max'];
-//                        $model->description = $property['desc'];
-//                        $model->title = $property['title'];
-//                        $model->phone = $property['telefon'];
-//                        $model->duties = $property['duties'];
-//                        $model->demands = $property['demands'];
-//                        $model->additionally = $property['additionally'];
-//                        $model->experience = (new TimeArray($property['experience_years'], null))->parce();
-//                        $model->education = (new Education($property['education'], null))->parce();
-//                        $model->schedule = (new Education($property['schedule'], null))->parce();
-//                        $model->salary_type = (new SalaryType($property['salary_type'] ?? null, null))->parce();
-//                        $model->alias = $slug;
-//                        $model->active = true;
-//                        $model->profile_id = $profileID;
-//                        if ($resume) {
-//                            $model->update();
-//                        } else {
-//                            $model->save();
-//                        }
-//                    }
-//                }
-//            }
-//        }
+        $client = new Client();
+        $response = $client->get('https://user.tapigo.ru/all-users-json', ['verify' => false]);
+        $contents = $response->getBody()->getContents();
+        $contents = json_decode($contents, true);
+        foreach ($contents as $item) {
+            $userDB = User::find($item['id']);
+            if (isset($userDB)) {
+                $user = $userDB;
+                $user->id = $item['id'];
+                $user->email = $item['email'];
+                $user->password = $item['password'];
+                $user->phone = $item['phone'];
+                $user->email_verified_at = $item['email_verified_at'];
+                $user->name = $item['profile'] ? $item['profile']['name'] : null;
+                $user->update();
+            } else {
+                $count = User::where('email', $item['email'])
+                    ->orWhere('phone', $item['phone'])
+                    ->get();
+
+                if ($count->count() === 0) {
+                    $user = new User();
+                    $user->id = $item['id'];
+                    $user->email = $item['email'];
+                    $user->password = $item['password'];
+                    $user->phone = $item['phone'];
+                    $user->email_verified_at = $item['email_verified_at'];
+                    $user->name = $item['profile'] ? $item['profile']['name'] : null;
+                    $user->save();
+                    $user->profile()->create(['id' => $item['profile']['id']]);
+                }
+            }
+        }
+
+        $client = new Client();
+        $response = $client->get('https://catalog.tapigo.ru/all-resume-json', ['verify' => false]);
+        $contents = $response->getBody()->getContents();
+        $contents = json_decode($contents, true);
+
+        foreach ($contents as $item) {
+            $userDB = User::find($item['id']);
+            if(!isset($userDB)) {
+                continue;
+            }
+            $profileID = $userDB->profile->getKey();
+            if (isset($item['posts']) && !empty($item['posts'])) {
+                foreach ($item['posts'] as $post) {
+                    if (isset($post['property']) && !empty($post['property'])) {
+                        if ($post['property_type'] === "App\\Models\\Jobs\\Resume\\JobsPostResumeProperty") {
+                            $property = $post['property'];
+                            $resume = JobsResume::find($property['id']);
+                            $slug = Str::slug($property['title'] .  ' ' . Str::random(5), '-');
+                            if ($resume) {
+                                $model = $resume;
+                            } else {
+                                $model = new JobsResume();
+                            }
+                            $model->id = $property['id'];
+                            $model->price = $property['price'];
+                            $model->description = $property['desc'];
+                            $model->title = $property['title'];
+                            $model->experience = (new TimeArray($property['experience_years'], null))->parce();
+                            $model->education = (new Education($property['education'], null))->parce();
+                            $model->schedule = (new Education($property['schedule'], null))->parce();
+                            $model->salary_type = (new SalaryType($property['salary_type'] ?? null, null))->parce();
+                            $model->alias = $slug;
+                            $model->active = true;
+                            $model->profile_id = $profileID;
+
+                            if ($resume) {
+                                $model->update();
+                            } else {
+                                $model->save();
+                            }
+                        }
+                    }
+                    if ($post['property_type'] === "App\\Models\\Jobs\\Vacancy\\JobsPostVacancyProperty") {
+                        $property = $post['property'];
+                        $resume = JobsVacancy::find($property['id']);
+                        $slug = Str::slug($property['title'] .  ' ' . Str::random(5), '-');
+                        if ($resume) {
+                            $model = $resume;
+                        } else {
+                            $model = new JobsVacancy();
+                        }
+                        $model->id = $property['id'];
+                        $model->min_price = $property['price'];
+                        $model->max_price = $property['price_max'];
+                        $model->description = $property['desc'];
+                        $model->title = $property['title'];
+                        $model->phone = $property['telefon'];
+                        $model->duties = $property['duties'];
+                        $model->demands = $property['demands'];
+                        $model->additionally = $property['additionally'];
+                        $model->experience = (new TimeArray($property['experience_years'], null))->parce();
+                        $model->education = (new Education($property['education'], null))->parce();
+                        $model->schedule = (new Education($property['schedule'], null))->parce();
+                        $model->salary_type = (new SalaryType($property['salary_type'] ?? null, null))->parce();
+                        $model->alias = $slug;
+                        $model->active = true;
+                        $model->profile_id = $profileID;
+                        if ($resume) {
+                            $model->update();
+                        } else {
+                            $model->save();
+                        }
+                    }
+                }
+            }
+        }
 
 
 
