3928a4c26d4bb9476bbce7f79d10f772efbe28b4
vasily.smolin <vasilysmoolin@gmail.com>
2022-04-07 15:25:46 +0400

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'name',
         'email',
+        'phone',
         'state',
         'password',
     ];
