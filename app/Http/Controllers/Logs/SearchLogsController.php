87bfab5a4d8c2aacf47cbfcd8b17948858a5d6ba
vasily.smolin <vasilysmoolin@gmail.com>
2022-09-08 19:23:45 +0400

    дата в логах

--- app/Http/Controllers/Logs/SearchLogsController.php
+++ app/Http/Controllers/Logs/SearchLogsController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers\Logs;
 use App\Http\Controllers\Controller;
 use App\Models\SearchLogs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class SearchLogsController extends Controller
 {
     public function index(Request $request)
     {
-
+        $from = Carbon::parse($request->get('dateFrom'))->format('Y-m-d');
+        $to = Carbon::parse($request->get('dateTo'))->format('Y-m-d');
         $logs = SearchLogs::select('id', 'text', 'type', 'created_at')
+            ->when(!empty($from) && !empty($to), function ($q) use ($from, $to) {
+                $q->whereBetween('created_at', [$from, $to]);
+            })
 //           ->where('type', 'catalog')
            ->get()
            ->groupBy('text')
